e341a590c7700bf319dc83a922507c574f8564f8
Joshua Hall <joshuahallmail@gmail.com>
2020-02-07 11:17:46 -0600

    Made test less redundant

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -16,12 +16,6 @@ suite =
     describe "reverseRange"
         [ fuzz2 (intRange 0 5000) (intRange 0 5000) "reverseRange should be equal to simpleReverseRange" <|
             \first second ->
-                if first >= second then
-                    reverseRange first second
-                        |> Expect.equalLists (simpleReverseRange first second)
-
-                else
-                    reverseRange first second
-                        |> List.isEmpty
-                        |> Expect.true "Should be empty if the hi is less than the low"
+                reverseRange first second
+                    |> Expect.equalLists (simpleReverseRange first second)
         ]
