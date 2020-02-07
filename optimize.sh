d97d252c2181d1dd3afa154d640cb2c587df7958
Joshua Hall <joshuahallmail@gmail.com>
2020-02-07 09:47:35 -0600

    Added CSS minification

--- optimize.sh
+++ optimize.sh
@@ -3,12 +3,21 @@
 set -e
 
 js="elm.js"
-min="elm.min.js"
+minJs="elm.min.js"
 
 elm make --optimize --output=$js $@
 
-uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$min
+uglifyjs $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$minJs
 
 echo "Compiled size:$(cat $js | wc -c) bytes  ($js)"
-echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
-echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
+echo "Minified size:$(cat $minJs | wc -c) bytes  ($minJs)"
+echo "Gzipped size: $(cat $minJs | gzip -c | wc -c) bytes"
+
+css="css/styles.css"
+minCss="css/styles.min.css"
+
+postcss $css > $minCss
+
+echo "Compiled size:$(cat $css | wc -c) bytes  ($css)"
+echo "Minified size:$(cat $minCss | wc -c) bytes  ($minCss)"
+echo "Gzipped size: $(cat $minCss | gzip -c | wc -c) bytes"
